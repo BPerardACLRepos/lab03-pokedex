42e10c039970d909dc8b416d0c82edc7221cedcd
Brandon Perard <brandon.perard@gmail>
2021-02-11 23:06:20 -0800

    add file structure with react layout on each page

---
+++ src/PokeList/PokeList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PokeItem from './PokeItem/PokeItem.js';
+
+export default class PokeList extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
---
+++ src/SearchPage/SearchCategory/SearchCategory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class SearchCategory extends React.Component {
+    render() {
+        return (
+
+    );
+    }
+}
